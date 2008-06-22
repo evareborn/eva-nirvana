a43fef06ea4c9dbdc52a879e63c992c56e959307
yunfan <yunfan_zg@163.net>
2008-06-22 04:24:16 +0000

    add the close bracket

--- src/ui/evacontactlistview.cpp
+++ src/ui/evacontactlistview.cpp
@@ -541,9 +541,10 @@ void EvaContactListView::updateContacts()
 	FriendList *list = EvaMain::user->getFriends();
 	QQFriend *f = list->firstFriend();
 	while( f ){
-		if (!m_contacts.count(f->getQQ()))	{
+		if (!m_contacts.count(f->getQQ())){
 			gIndex = m_groups.count(f->getGroupIndex())?f->getGroupIndex():0;
 			m_contacts[f->getQQ()] = new EvaBuddyItem(*f, m_groups[gIndex]);
+		}
 
 		f = list->nextFriend();
 	}
